b66aa48035c30bbeab3e25a29423bde62d93b265
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2023-12-27 11:19:09 +0530

    styles

--- src/login.js
+++ src/login.js
@@ -1,5 +1,17 @@
+import "./login.css";
 const Login = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="login">
+      <form className="form">
+        <h2>Welcome to library management system</h2>
+        <input type="text" placeholder="username"></input>
+        <input type="text" placeholder="password"></input>
+        <button>LoginIn</button>
+        <text>forget your password?</text>
+        <text>wanna Sign Up?</text>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
