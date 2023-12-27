1ce7751a3f3fb12890d4378899ab7ad0e909e358
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2023-12-27 14:42:51 +0530

    Implement login

--- src/login.js
+++ src/login.js
@@ -2,17 +2,44 @@ import { useState } from "react";
 import "./login.css";
 
 const Login = () => {
+  const correctUsername = "ayeshan";
+  const correctPassword = "ayeshan";
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [message, setMessage] = useState();
+
+  const handleUsername = (event) => {
+    setUsername(event.target.value);
+  };
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
   const handleClick = () => {
-    setMessage("wrong username or password");
+    if (username === correctUsername && password === correctPassword) {
+      setMessage("Welcome");
+    } else {
+      setMessage("wrong username or password");
+    }
   };
 
   return (
     <div className="login">
       <div className="form">
         <h2>Welcome to the library management system</h2>
-        <input type="text" placeholder="username" />
-        <input type="text" placeholder="password" />
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={handleUsername}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={handlePassword}
+        />
         <span className="error">{message}</span>
         <button onClick={handleClick}>Login</button>
         <span className="link">Forget your password?</span>
